e1048d113d785478737aa7654d0230fccd8e755f
Roman Kontsevoi <3844320@gmail.com>
2019-06-20 14:04:43 +0300

    parallax attempt

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,16 @@ import '../css/reset.css';
 import '../scss/style.scss';
 import '../scss/responsive.scss';
 import Vue from 'vue';
+import simpleParallax from 'simple-parallax-js';
 
 var app = new Vue({
     el: '#app',
     data: {
         message: 'Привет, Vue!'
     }
+});
+
+var images = document.querySelectorAll('.parallax');
+var instance = new simpleParallax(images, {
+    scale: 1.7
 });
