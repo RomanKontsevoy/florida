1b8cfa00e8f04fa09d7e711a17018f09f69774f6
Roman Kontsevoi <3844320@gmail.com>
2019-06-20 18:51:11 +0300

    video translate -10% on adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,7 @@
 import '../css/reset.css';
 import '../scss/style.scss';
 import '../scss/responsive.scss';
-import Vue from 'vue';
 import simpleParallax from 'simple-parallax-js';
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Привет, Vue!'
-    }
-});
 
 var images = document.querySelectorAll('.parallax');
 var instance = new simpleParallax(images, {
